1dc67b8de3de10878682d5923e51f2db52db0bdc
Eng Ahmed Waked <eng.ahmed.a.waked@gmail.com>
2025-10-26 02:23:00 +0300

    Fix Flask port for Deployra

--- backend/app.py
+++ backend/app.py
@@ -1279,5 +1279,5 @@ if __name__ == '__main__':
         logger.info("=" * 70)
     
     # Run the application
-    port = int(os.environ.get("PORT", 5000))
-    app.run(debug=(env == 'development'), host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(debug=(env == 'development'), host='0.0.0.0', port=port)
